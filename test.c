33492b3102d3cd1cafc2298cb8781d378cd38785
Gianni Henriques <gianni.henriques@epitech.eu>
2023-03-28 22:50:28 +0200

    [~] test without cstyle errors

--- test.c
+++ test.c
@@ -1,4 +1,12 @@
-int main(void){
-    int a= 1;
+/*
+** EPITECH PROJECT, 2023
+** my_petit_pain_au_chocolatine
+** File description:
+** test
+*/
+
+int main(void)
+{
+    int a = 1;
     return 0;
 }
